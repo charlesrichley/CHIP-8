10d3ac7dcb032989cd532ca013ccad39099466ef
Charles Richley <richleycharlie@gmail.com>
2026-08-25 12:58:34 +0100

    Add Button struct and update function signatures

--- src/header.h
+++ src/header.h
@@ -18,6 +18,7 @@
 #define INSRUCTIONS_PER_FRAME 12
 
 #define MAX_FILE_NAME_SIZE 2000
+#define QUIRK
 #define NUMBER_OF_QUIRKS 8
 
 typedef struct 
@@ -40,6 +41,16 @@ typedef struct
     int length; 
 } Input_String;
 
+typedef struct
+{
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+    bool is_on;
+    char *string;
+    int x;
+    int y;
+} Button;
+
 enum {
     RESET_VF,
     MEMORY,
@@ -56,16 +67,15 @@ extern char keyboard_arr[16];
 extern SDL_Scancode scancode_arr[16];
 extern uint8_t sprite_arr[80];
 
-bool is_empty(Stack *stack);
 void initialise(Stack *stack);
 void push(Stack *stack, uint16_t new_value);
 int pop(Stack *stack);
 
-SDL_FRect get_frect_TL(int i, int j, int w, int h);
-SDL_FRect get_frect_centered(int i, int j, int w, int h);
-void pixels_on(uint8_t pixels[WIDTH][HEIGHT]);
+SDL_FRect get_frect(int i, int j, int w, int h, bool is_centered);
 
 int keyboard_to_index(char c);
 int scancode_to_index(Keypad keypad[16], SDL_Scancode scancode);
 int chip_8_to_keyboard(int chip_8);
 int keyboard_to_chip_8(int keyboard);
+
+Button get_button(int x, int y, char *string, bool is_on, TTF_Font *font, SDL_Color font_color, SDL_Renderer *renderer);
